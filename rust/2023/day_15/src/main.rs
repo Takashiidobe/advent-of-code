06e2f1c05b0aa5c5c93f6fb9bc59a356de4d22d8
Takashi Idobe <mail@takashiidobe.com>
2023-12-16 19:11:34 -0500

    day 15 part 2

--- rust/2023/day_15/src/main.rs
+++ rust/2023/day_15/src/main.rs
@@ -1,11 +1,13 @@
+use linked_hash_map::LinkedHashMap;
+
 fn parse(input: &str) -> Vec<&str> {
     input.split(',').collect()
 }
 
-fn hash_one(bytes: &[usize]) -> usize {
+fn hash_one(bytes: &[u8]) -> usize {
     let mut hash_val = 0;
     for b in bytes {
-        hash_val += b;
+        hash_val += *b as usize;
         hash_val *= 17;
         hash_val %= 256;
     }
@@ -16,22 +18,51 @@ fn part_1(input: &str) -> usize {
     let parsed = parse(input);
     let mut total = 0;
     for s in parsed {
-        let bytes: Vec<usize> = s.bytes().map(|b| b as usize).collect();
+        let bytes: Vec<_> = s.bytes().collect();
         total += hash_one(&bytes);
     }
     total
 }
 
-// add to the data structure, a vector of boxes from 0 -> 255
+fn part_2(input: &str) -> usize {
+    let parsed = parse(input);
+    let mut boxes = vec![LinkedHashMap::<&str, usize>::new(); 256];
 
+    for val in parsed {
+        let split: Vec<_> = val.split(|s| s == '-' || s == '=').collect();
+        let tag_hash = hash_one(split[0].as_bytes());
+        if val.contains('=') {
+            *boxes[tag_hash].entry(split[0]).or_default() = split[1].parse().unwrap();
+        }
+        if val.contains('-') {
+            boxes[tag_hash].remove(split[0]);
+        }
+    }
+    let mut total = 0;
+
+    for (box_index, m) in boxes.into_iter().enumerate() {
+        for (map_index, v) in m.values().enumerate() {
+            total += (box_index + 1) * (map_index + 1) * v;
+        }
+    }
+
+    total
+}
+
+// add to the data structure, a vector of boxes from 0 -> 255
 fn main() {
-    let example = include_str!("../example.txt").trim();
     let input = include_str!("../input.txt").trim();
-    println!("{}", part_1(input));
+    println!("part 1: {}, part 2: {}", part_1(input), part_2(input));
 }
 
 #[test]
 fn example_1() {
     let example = include_str!("../example.txt").trim();
     assert_eq!(1320, part_1(example));
+}
+
+#[test]
+fn example_2() {
+    let example = include_str!("../example.txt").trim();
+    assert_eq!(145, part_2(example));
 }
